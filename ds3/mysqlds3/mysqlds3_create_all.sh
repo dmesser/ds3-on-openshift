b1fb5b6dc7b95895288cfa831485bb526b0804d8
dmesser <dmesser@redhat.com>
2017-11-20 10:49:41 +0100

    refer to env vars correctly

--- ds3/mysqlds3/mysqlds3_create_all.sh
+++ ds3/mysqlds3/mysqlds3_create_all.sh
@@ -3,31 +3,31 @@
 cd build
 echo Warning messgaes about using password via command line are normal......
 echo Creating database
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_db.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_db.sql
 echo Creating Indexes
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_ind.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_ind.sql
 echo Creating Stored Procedures
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_sp.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_create_sp.sql
 cd ../load/cust
 echo Loading Customer Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_cust.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_cust.sql
 cd ../orders
 echo Loading Orders Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_orders.sql 
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_orders.sql
 echo Loading Orderlines Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_orderlines.sql 
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_orderlines.sql
 echo Loading Customer History Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_cust_hist.sql 
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_cust_hist.sql
 cd ../prod
 echo Loading Products Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_prod.sql 
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_prod.sql
 echo Loading Inventory Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_inv.sql 
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_inv.sql
 cd ../membership
 echo Loading Membership data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_member.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_member.sql
 cd ../reviews
 echo Loading Reviews data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_reviews.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_reviews.sql
 echo Loading Reviews Helpfulness Ratings Data
-mysql --user={$MYSQL_USER} --password={$MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_review_helpfulness.sql
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} < mysqlds3_load_review_helpfulness.sql
